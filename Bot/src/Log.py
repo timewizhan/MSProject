83bb639a0f27842484c5e412c2251f5ccdf44e40
timewizhan <timewizhan@gmail.com>
2016-01-12 22:57:04 +0900

    [MSProject_Bot][timewizhan][RV:N/A] 1. 의미 없이 추가 된 코드 삭제

--- Bot/src/Log.py
+++ Bot/src/Log.py
@@ -35,9 +35,3 @@ class Log:
 
 
 
-
-import logging
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
