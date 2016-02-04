e6651d650dcb878bf89071f3f6cd786886f3ac37
alphahacker <alphahacker.han@gmail.com>
2016-02-04 19:30:11 +0900

    [MSProject_EntryPoint][alphahacker][RV:N/A]

---
+++ EntryPoint/Common.h
@@ -0,0 +1,25 @@
+/*
+ * Common.h
+ *
+ *  Created on: Jan 23, 2016
+ *      Author: alphahacker
+ */
+
+#ifndef _COMMON_
+#define _COMMON_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#endif
+
+
---
+++ EntryPoint/MainEP.cpp
@@ -0,0 +1,18 @@
+/*
+ * MainEP.cpp
+ *
+ *  Created on: Jan 23, 2016
+ *      Author: alphahacker
+ */
+
+#include "Common.h"
+#include "EP_Test4.h"
+
+int main()
+{
+	EP_Test4 et;
+	et.initEntryPoint();
+//	et.sendMessage();
+}
+
+
